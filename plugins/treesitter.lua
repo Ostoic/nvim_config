dc587139f9c943caf6699fdb73e7933dc1bdd841
Shaun Ostoic <8475295+Ostoic@users.noreply.github.com>
2023-04-22 17:14:52 -0400

    Update

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "rust", "python" },
+    ensure_installed = { "lua", "rust", "python", "c", "vim", "bash", "toml", "html", "css", "markdown"},
   },
 }
